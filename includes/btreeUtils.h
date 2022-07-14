86ec510c727bd5d10f04e62d5d286d26a4b60bc2
Theo Riffel <theobfriffel@gmail.com>
2022-07-14 15:52:28 -0300

    ponto agas

--- includes/btreeUtils.h
+++ includes/btreeUtils.h
@@ -12,10 +12,16 @@ void escreverCabecalhoBTree(FILE* BTree_file, cabecalhoBTree_t* cabecalho, int t
 cabecalhoBTree_t* lerCabecalhoBTree(FILE* BTree_file, int tipo);
 registroBTree_t *inicializarRegistroBTree();
 void liberarRegistroBTree(registroBTree_t* no);
-registroBTree_t* inicializarBTree();
-void escreverRegistroBTree(FILE* BTree_file, registroBTree_t* no, int tipo);
+chave_t retiraChave();
+registroBTree_t* inicializarBTree(FILE* BTree_file, int tipo);
+void escreverRegistroBTree(FILE* BTree_file, int RRN, registroBTree_t* no, int tipo);
 void printaRegistroBTree(registroBTree_t *no, int tipo);
 registroBTree_t* lerRegistroBTree(FILE* BTree_file, int tipo);
 chave_t* searchBTree(FILE* BTree_file, int RRN_no, int chave, int tipo);
+bool verificaNoFolha(registroBTree_t *no);
+bool verificaTaxaOcupacao(registroBTree_t *no);7
+void insertChaveNo(chave_t chave, registroBTree_t *no, int RRN_abaixo, int tipo);
+void split2(cabecalhoBTree_t *cab, chave_t key, int r_child, registroBTree_t *no, chave_t *promo_key, int *promo_r_child, registroBTree_t *novoNo);
+bool insert2(cabecalhoBTree_t *cab, FILE *BTree_file, int RRN_no, chave_t chave, int *promo_r_child, chave_t *promo_key, int tipo);
 
 #endif
